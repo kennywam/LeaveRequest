7bf5937d29fa77b71bf9447269cc36f360872d32
Kennedy Wambua <kennywambu254@gmail.com>
2022-07-26 16:58:39 +0300

    adding leaveRequest webpart

---
+++ src/webparts/leaveRequest/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ILeaveRequestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'LeaveRequestWebPartStrings' {
+  const strings: ILeaveRequestWebPartStrings;
+  export = strings;
+}
